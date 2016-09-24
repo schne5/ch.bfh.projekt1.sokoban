14a838d3c8ad9a35321c84ceedbe2056669f677c
schne5 <elisa.schnabel@gmx.ch>
2016-09-24 14:44:13 +0200

    Projekt struktur hinzugefügt

---
+++ src/main/java/ch/bfh/projekt1/sokoban/sokoban/App.java
@@ -0,0 +1,13 @@
+package ch.bfh.projekt1.sokoban.sokoban;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
